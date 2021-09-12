0dce45f66597988e7ca412b9d79cd7781bc6a50c
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2021-09-12 17:47:54 -0400

    fix user type error

--- src/pages/api/company/[email].ts
+++ src/pages/api/company/[email].ts
@@ -1,10 +1,10 @@
 import prisma from '../../../../lib/prisma';
-import { User } from '../../../../lib/user';
+import { UserProps } from '../../../../lib/user';
 
 export default async function handle(req, res) {
     const { email } = req.query
 
-    const user: User = await prisma.user.findFirst({
+    const user: UserProps = await prisma.user.findFirst({
         where: {
             email: email
         },
